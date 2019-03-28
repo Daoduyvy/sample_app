5db67642392e5dcba7a1c36df12533b7d7c53179
Daoduyvy <mrduyvivbhp@gmail.com>
2019-03-28 20:28:31 +0700

    user-microposts

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ User.create!(name:  "Dao Duy Vy",
              password:              "dvi180595",
              password_confirmation: "dvi180595",
              admin:     true,
-             activated: false
+             activated: true
              )
 
 99.times do |n|
@@ -23,4 +23,9 @@ User.create!(name:  "Dao Duy Vy",
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
